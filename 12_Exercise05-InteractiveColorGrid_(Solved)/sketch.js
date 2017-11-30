3733730415b104a042ac64490fe847fa8ae1a57d
(s) Brieanna Mulvey <bmulvey@pm7-0096.uopnet.plymouth.ac.uk>
2017-11-30 14:47:49 +0000

    wepage made

---
+++ 12_Exercise05-InteractiveColorGrid_(Solved)/sketch.js
@@ -0,0 +1,30 @@
+
+
+let tileSize = 50;
+
+function setup() {
+  var canvas = createCanvas(500, 500); //Use this function to define the size of the output window
+  stroke(255);
+
+  var text = createDiv("This is an HTML string");
+  text.position(50,50);
+
+  canvas.parent("myContainer");{
+
+  frameRate(20);
+
+function draw() {
+
+for (var i = 0; i < 10; i++) {
+
+  stroke(1);
+  fill(random(255),random(255),random(255))
+
+  ellipse(random(500), random(550), random(50), random (50));
+  rect(random(500), random(550), random(50), random (50));
+
+  fill (random(255), random(255), random (255));
+  ellipse(random(500), random(500), random(50), random (50));
+  }
+
+}}
--- geometric universe/sketch.js
+++ geometric universe/sketch.js
@@ -1,31 +1,29 @@
 
 function setup() {
 
-  createCanvas(500,500);
+  var canvas = createCanvas(500,500);
+  canvas.parent("myContainer");
 //makes shapes re randomise every 1 second
-  frameRate(5000);
+  frameRate(20);
 }
 function draw() {
 //depending on what position you put the letter that you have chosen to be random, will randomise between the number you have given e.g 500
 //so putting r in position (_, r, _, _) then the y axis will be variable and rndom between 500
-for (var i = 0; i < 900; i++) {
-  var r = random(500);
-  var s = random(550);
-  var t = random(50);
-  var j = random (50);
-  stroke(1);
-  fill(r,s,t)
-  ellipse(r, s, t, j);
-    var rr = random(500);
-    var rs = random(550);
-    var rt = random(50);
-    fill(rr,rs,rt)
-  rect(rr, rs, rt, 50);
+// for this condition if i=0, if 0 is less than 10 then add 1(++)
+//== is equal to this values
+//= equals
+//++ add one
+//+=
+for (var i = 0; i < 10; i++) {
 
-for (let x = 0; x< 100; x++) {
-let size = random (5, 100);
-fill (random(255), random(255), random (255));
-  ellipse(random(width), random(height), size, size)
-}
-}
+  stroke(1);
+  fill(random(255),random(255),random(255))
+
+  ellipse(random(500), random(550), random(50), random (50));
+  rect(random(500), random(550), random(50), random (50));
+
+  fill (random(255), random(255), random (255));
+  ellipse(random(500), random(500), random(50), random (50));
+  }
+
 }
